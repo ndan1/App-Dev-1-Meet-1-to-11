52eb17171de35816b5d6be68eb4bb39d5ef456ed
ndan1 <110277508+ndan1@users.noreply.github.com>
2022-08-06 16:24:49 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-// import TryCodeScreen from './src/screens/TryCodeScreen';
-// import HomeScreen from './src/screens/HomeScreen';
-import MainNavigator from './src/navigator/MainNavigator';
+import React from "react";
+import 'react-native-gesture-handler';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import MainNavigator from "./src/navigator/MainNavigator";
+// import HomeScreen from "./src/screens/HomeScreen";
+// import GuessTheCountryScreen from "./src/screens/GuessTheCountryScreen";
+import WinScreen from "./src/screens/WinScreen";
 
 const App = () => {
   return (
-  <SafeAreaProvider>
-    <MainNavigator/>
-  </SafeAreaProvider>
+    <SafeAreaProvider>
+      <MainNavigator/>
+    </SafeAreaProvider>
   )
 };
 
--- src/navigator/MainNavigator.js
+++ src/navigator/MainNavigator.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "../screens/HomeScreen";
+import GuessTheCountryScreen from "../screens/GuessTheCountryScreen";
+import WinScreen from "../screens/WinScreen";
+import GameOverScene from "../screens/GameOverScreen";
 
 const Stack = createStackNavigator();
 
 const MainNavigator = () => {
-    return(
+    return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
+            <Stack.Navigator initialRouteName="GameOver">
                 <Stack.Screen
                     name="Home"
-                    component={HomeScreen}/>
+                    component={GuessTheCountryScreen}
+                    options={{ headerShown: false}}/>
+                <Stack.Screen
+                    name="Win"
+                    component={WinScreen}
+                    options={{ headerShown: false}}/>
+                <Stack.Screen
+                    name="GameOver"
+                    component={GameOverScene}
+                    options={{ headerShown: false}}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
